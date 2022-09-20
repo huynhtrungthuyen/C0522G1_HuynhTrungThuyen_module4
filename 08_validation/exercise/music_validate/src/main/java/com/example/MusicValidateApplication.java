15eb2efc6c6723a46316bd582b9adfc65abdd57b
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-09-20 16:02:39 +0700

    8

--- 08_validation/exercise/music_validate/src/main/java/com/example/MusicValidateApplication.java
+++ 08_validation/exercise/music_validate/src/main/java/com/example/MusicValidateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ValidatingFormInputApplication {
+public class MusicValidateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ValidatingFormInputApplication.class, args);
+        SpringApplication.run(MusicValidateApplication.class, args);
     }
 
 }
